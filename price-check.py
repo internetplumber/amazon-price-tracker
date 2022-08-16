d3ff8ffb1adf0e690a09451ad0c5227f0d520e8e
Rob Evans <rhe@internetplumber.org.uk>
2022-08-16 22:09:34 +0100

    Substantial changes to cope with different id to look for, and handle duplicate prices in the return.

--- price-check.py
+++ price-check.py
@@ -17,13 +17,23 @@
 #   "https://www.amazon.co.uk/Govee-Thermometer-Hygrometer-Temperature-Greenhouse/dp/B086YYL439/",
 #   "https://www.amazon.co.uk/Govee-Thermometer-Hygrometer-Temperature-Greenhouse/dp/B08XQD8MFZ/"
 # ]
+#
+# This is currently very UK-specific. It uses the GBP symbol to look for the
+# price, and normalises URLs based on amazon.co.uk.
 
-
+import random
 import requests
 import re
 import json
 import os
 from bs4 import BeautifulSoup
+
+userAgents = [ "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:93.0) Gecko/20100101 Firefox/93.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Safari/605.1.15",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36 Edg/94.0.992.47",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36" ]
 
 productPrices = {}
 lastProdPrices = {}
@@ -40,7 +50,15 @@ productFile.close
 
 
 def get_converted_price(price):
-    converted_price = float(re.sub(r"[^\d.]", "", price))
+    # This function has got a bit ugly as the primary id we're now using to
+    # get the prices returns the price twice, so the first thing we need to
+    # look for is exactly one copy of it -- note this keys on the GBP symbol
+    # and should look for other currency symbols.
+    price1 = re.search("£[\d,.]+", price)
+    # Now we strip anyghing else our.  This is a hangover from the previous
+    # way the function works, and practically all it is doing now is stripping
+    # out the comma when the price is £1,000 or over.
+    converted_price = float(re.sub(r"[^\d.]", "", price1.group(0)))
     return converted_price
 
 
@@ -66,7 +84,7 @@ def get_product_details(url):
     # ToDo: add a few different User-Agent strings that can be tried at random
     # to try and avoid captchas because we look like the robot that we are.
     headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Safari/605.1.15"
+        "User-Agent": random.choice(userAgents)
     }
     details = {"name": "", "price": 0, "deal": True, "url": ""}
     _url = extract_url(url)
@@ -76,7 +94,7 @@ def get_product_details(url):
         page = requests.get(_url, headers=headers)
         soup = BeautifulSoup(page.content, "html5lib")
         title = soup.find(id="productTitle")
-        priceLocators = [ "newBuyBoxPrice", "price_inside_buybox", "priceblock_dealprice", "priceblock_ourprice"]
+        priceLocators = [ "corePrice_feature_div"]
         for priceId in priceLocators:
             price = soup.find(id=priceId)
             if price is not None:
@@ -85,6 +103,7 @@ def get_product_details(url):
             # Can't locate a price, see what the webpage says (it's
             # probably Amazon checking we're not a robot)...
             print("Unable to get price for " + url)
+            #print(page.content)
         if title is not None and price is not None:
             details["name"] = title.get_text().strip()
             details["price"] = get_converted_price(price.get_text())
